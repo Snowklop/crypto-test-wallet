9a80d61daba2bb414da76d219c71a6e372df80a7
aprimak <primaksanya@gmail.com>
2022-01-14 14:39:10 +0100

    Fixed husky hooks

--- src/components/LoadingContent.tsx
+++ src/components/LoadingContent.tsx
@@ -1,22 +1,14 @@
-import { CircularProgress, Grid } from "@mui/material";
-import { PropsWithChildren } from "react";
+import { CircularProgress, Grid } from '@mui/material';
+import { PropsWithChildren } from 'react';
 
 type Props = {
   isLoading: boolean;
 };
 
-export const LoadingContent = ({
-  isLoading,
-  children,
-}: PropsWithChildren<Props>) => (
+export const LoadingContent = ({ isLoading, children }: PropsWithChildren<Props>) => (
   <>
     {isLoading ? (
-      <Grid
-        container spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-      >
+      <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center">
         <CircularProgress />
       </Grid>
     ) : (
